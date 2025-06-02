bec9e45b95275b77be7ba7289b926fd3d45d4769
enrate <80484441+enrate@users.noreply.github.com>
2025-06-02 21:09:53 +0300

    Update db.ts

--- db.ts
+++ db.ts
@@ -1,11 +1,11 @@
 import mysql from "mysql2/promise";
 
 export default mysql.createPool({
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '3306'), // Порт отдельно
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQL_HOST,
+  port: parseInt(process.env.MYSQL_PORT || '3306'), // Порт отдельно
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DBNAME,
   waitForConnections: true,
   connectionLimit: 10,
   multipleStatements: true, // Разрешаем несколько запросов в одном

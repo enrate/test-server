21c4b86bc4fe5c2d391e6b6e6538de0132690ce2
enrate <80484441+enrate@users.noreply.github.com>
2025-06-02 21:07:38 +0300

    test

---
+++ routes/get-users.ts
@@ -0,0 +1,39 @@
+import pool from "../db"
+
+export default async function getUsers({userId, email}: {userId: any, email: any}){
+    const connection = await pool.getConnection();
+          try {
+            await connection.beginTransaction();
+                 let query = 'SELECT * FROM users';
+        const params: any[] = [];
+        const conditions: any[] = [];
+        
+        // Добавляем фильтры, если они переданы
+        if (userId !== undefined) {
+            conditions.push('id = ?');
+            params.push(userId);
+        }
+        
+        if (email !== undefined) {
+            conditions.push('email = ?');
+            params.push(email);
+        }
+        
+        // Добавляем WHERE если есть условия
+        if (conditions.length > 0) {
+            query += ' WHERE ' + conditions.join(' AND ');
+        }
+        
+        // Выполняем запрос
+        const [users] = await connection.query(query, params);
+        await connection.commit();
+        return users;
+            } catch (error) {
+                await connection.rollback();
+                throw error;
+            } finally {
+                connection.release();
+        }
+
+}
+//module.exports = { getUsers };
---
+++ server.ts
@@ -0,0 +1,58 @@
+require('dotenv/config');
+import express from "express"
+const app = express();
+import getUsers from "./routes/get-users";
+import postUsers from "./routes/post-users";
+
+// Middleware для разбора JSON
+app.use(express.json());
+
+// Конфигурация
+const PORT = 3003;
+const API_TOKEN = 'dkfSkell35jwlslSL';
+
+
+// Middleware проверки токена
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ error: 'Требуется авторизация' });
+  }
+
+  const token = authHeader.split(' ')[1];
+  
+  if (token !== API_TOKEN) {
+    return res.status(403).json({ error: 'Неверный токен' });
+  }
+
+  next();
+};
+
+app.get("/users", async (req, res) => {
+  try {
+    const filters = {
+            userId: req.query.userId,
+            email: req.query.email
+        };
+
+     const users = await getUsers(filters);
+     res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message})
+  }
+})
+
+app.post("/users", authMiddleware, async (req, res) => {
+  try {
+    const ss = await postUsers(req.body)
+    res.json(ss)
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+})
+
+// Запуск сервера и Discord бота
+  app.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
+  });
